8ca78af25372c63fc52134b7693e949b6fac40d0
VShoji <69880007+VShoji@users.noreply.github.com>
2022-11-30 11:56:16 -0300

    Update AdjacencyMatrixGraph.java

--- app/src/main/java/br/unicamp/marstransport/dataStructures/graphs/AdjacencyMatrixGraph.java
+++ app/src/main/java/br/unicamp/marstransport/dataStructures/graphs/AdjacencyMatrixGraph.java
@@ -8,11 +8,40 @@ import java.util.List;
 import br.unicamp.marstransport.dataStructures.matrices.KeyMatrix;
 
 public class AdjacencyMatrixGraph<K, N extends Number> {
-    KeyMatrix<K, N> matrix;
-    List<K> nodes;
+    private KeyMatrix<K, N> matrix;
+    private List<K> nodes;
+
+    public static enum Algorithm {
+        DJIKSTRA,
+        BACKTRACKING
+    }
 
     public AdjacencyMatrixGraph(@NonNull List<K> nodes) throws KeyException {
         this.matrix = new KeyMatrix<K, N>(nodes);
         this.nodes = nodes;
+    }
+
+    public N getWeight(K row, K col) {
+        return matrix.getValue(row, col);
+    }
+
+    public void setWeight(N weight, K row, K col) {
+        matrix.setValue(weight, row, col)
+    }
+
+    public Queue<K> getShortestPath(K start, K dest) {
+        return djikstra(start, dest);
+    }
+
+    public Queue<K> getShortestPath(K start, K dest, Algorithm algorithm) {
+        throw new NotImplementedException();
+    }
+
+    private Queue<K> djikstra(K start, K dest) {
+        Matrix<N> distance = new Matrix<N>(nodes.count())
+        int startIdx = nodes.indexOf(start)
+        distance.setValue(0, startIdx, startIdx);
+
+        throw new NotImplementedException();
     }
 }
